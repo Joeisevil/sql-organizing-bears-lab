e32e74a0155735ebda9eb088060bf73fcc9ee36f
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-02-08 18:04:22 -0500

    commit tree c5341a2c99a8f13343b8c8bb3acd296896372743

---
+++ lib/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE bears (
+    column	type,
+    id	INTEGER PRIMARY KEY,
+    name	text,
+    age	integer,
+    sex	text,
+    color	text,
+    temperament	text,
+    alive boolean
+); 
+
+
+
+
---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 41, 'M', 'brown', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 41, 'M', 'brown', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 41, 'M', 'brown', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 41, 'M', 'brown', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 41, 'M', 'brown', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 41, 'M', 'brown', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 41, 'M', 'brown', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 41, 'M', 'brown', 'nice', 0);
+
+
+
+
+
+
+
+
+
